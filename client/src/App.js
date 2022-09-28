47f8b0a90168e92d2b7c446c9b28ad094a97752f
etuong <etuong@gmail.com>
2022-09-28 13:38:20 -0700

    Make client background color dynamic

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 
 function App() {
+  const backgroundColor = process.env.REACT_APP_COLOR ?? "#282c34";
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" style={{ backgroundColor }}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Create React App to demo Nginx Authentication</p>
       </header>
